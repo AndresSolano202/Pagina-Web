3af8ecc2a4d8e6a746c1608b9087b8e5b84449ca
Andress <andres.solanov.est@uets.edu.ec>
2022-11-21 12:36:14 -0500

    Blog 9

--- PagWeb/src/Componentes/modulo1/componente1/componente1.component.ts
+++ PagWeb/src/Componentes/modulo1/componente1/componente1.component.ts
@@ -4,7 +4,8 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-componente1',
   templateUrl: './componente1.component.html',
-  styleUrls: ['./componente1.component.css']
+  styleUrls: ['./componente1.component.css'],
+  
 })
 export class Componente1Component implements OnInit{
   constructor(private router: Router) { }
