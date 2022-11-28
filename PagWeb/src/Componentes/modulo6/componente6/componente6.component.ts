7d5ee38802218565af24ab426034b78b709f0895
Andress <andres.solanov.est@uets.edu.ec>
2022-11-27 21:27:08 -0500

    animaciones

--- PagWeb/src/Componentes/modulo6/componente6/componente6.component.ts
+++ PagWeb/src/Componentes/modulo6/componente6/componente6.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { state, style, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-componente6',
   templateUrl: './componente6.component.html',
-  styleUrls: ['./componente6.component.css']
+  styleUrls: ['./componente6.component.css'],
+  animations: [
+    trigger('Cuadroanimacion', [
+      state('estado1', style({
+        backgroundColor: 'red'
+      })),
+      state('estado2', style({
+        backgroundColor: 'blue'
+      }))
+    ])
+    
+  ]
 })
 export class Componente6Component implements OnInit {
+  estadoCuadro = 'estado1';
   constructor(private router: Router) { }
 
   ngOnInit(): void {
@@ -14,6 +27,11 @@ export class Componente6Component implements OnInit {
   navegacion(){
     this.router.navigate([''])
    
+  }
+
+  animar()
+  {
+    this.estadoCuadro = this.estadoCuadro === 'estado1' ? 'estado2' : 'estado1';
   }
 
 }
